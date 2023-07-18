8a7f91442f961d7e961307f409234eea41f0bcb9
OblivionNoirV2 <donahueb85@gmail.com>
2023-07-17 20:15:00 -0500

    misc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,11 +67,11 @@ const About = () => {
       Music has always been a deep love of mine since I was little,
       so...here I am fullfilling one of my biggest dreams. I take a lot
       of inspiration from video game music, composers such as
-      <a href='https://open.spotify.com/track/4aPltIjGkJx26Y9cBHu14U?si=66d4d060c0ec4fec'>Yoko Shimomura</a>
-      and <a href='https://open.spotify.com/track/65CefKbmaG0ZkumWSFKbxC?si=7c7af95103094401'>Yuka Kitamura</a>
+      <a href='https://open.spotify.com/track/4aPltIjGkJx26Y9cBHu14U?si=66d4d060c0ec4fec'> Yoko Shimomura </a>
+      and <a href='https://open.spotify.com/track/65CefKbmaG0ZkumWSFKbxC?si=7c7af95103094401'>Yuka Kitamura </a>
       being a couple of my biggest inspirations along with
-      other artists like <a href='https://open.spotify.com/track/6rMRUcrL63WNnhMPYew7pA?si=17d1a83abb8d437f'>MGLA</a> and
-      <a href='https://open.spotify.com/track/4m4Yj2z7CG1LUNbcg1uOP3?si=bd5d217dd6614e89'>Perturbator.</a>
+      other artists like <a href='https://open.spotify.com/track/6rMRUcrL63WNnhMPYew7pA?si=17d1a83abb8d437f'>MGLA </a> and
+      <a href='https://open.spotify.com/track/4m4Yj2z7CG1LUNbcg1uOP3?si=bd5d217dd6614e89'> Perturbator. </a>
       Dark and beautiful with a story in mind is how I compose.
       This site doesn't have much now but it will grow in the future.
     </p>
